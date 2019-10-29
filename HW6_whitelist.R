93002f72c6b2b2b708740eb62b832ac2fa769c82
merliseclyde <clyde@stat.duke.edu>
2019-10-28 23:09:43 -0400

    initial commit

---
+++ HW6_whitelist.R
@@ -0,0 +1,22 @@
+allowed_files = c("HW6.Rmd",
+                  "HW6.pdf",
+                  "HW6.html",
+                  "README.md",
+                  "candy-data.csv",
+                  "candy-EDA.Rmd",
+                  "HW6_whitelist.R",
+                  "wercker.yml")
+
+files = dir()
+disallowed_files = files[!(files %in% allowed_files)]
+
+if (length(disallowed_files != 0))
+{
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in disallowed_files)
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
